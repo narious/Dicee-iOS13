f4864e567e74bece7a9a29401304383ff405f72e
Michael Dinh <tonkoalsce@gmail.com>
2020-11-25 23:50:25 +1100

    Finished dice rolls project

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,25 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     
     let diceArr = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ]
-    var leftDiceNumber = 0
-    var rightDiceNumber = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //Attributes is the down pencil thing
-        //Using image literals
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageView1.alpha = CGFloat(0.5)
-    }
     
     //Changes the dice image
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        
-        diceImageView1.image =  diceArr[leftDiceNumber]
-        diceImageView2.image =  diceArr[rightDiceNumber]
+        diceImageView1.image =  diceArr[Int.random(in: 0...5)]
+        diceImageView2.image =  diceArr[Int.random(in: 0...5)]
     }
     
 }
